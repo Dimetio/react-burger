8c6f2423a21834822710ff6934bbdbbd660e79a1
ddubinin <dubinin.dmitry@mail.ru>
2022-10-28 18:49:54 +0300

    create structre

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import './App.css';
+// components
+import AppHeader from './components/app-header/app-header';
+import BurgerConstructor from './components/burger-constructor/burger-constructor';
+import BurgerIngredients from './components/burger-ingredients/burger-Ingredients';
+// styles
+import styles from './app.module.css';
+// utils
+import ingredients from './utils/data';
+
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients ingredients={ingredients} />
+        <BurgerConstructor ingredients={ingredients}/>
+      </main>
     </div>
   );
 }
