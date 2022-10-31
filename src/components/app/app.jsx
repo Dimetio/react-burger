4680799014dc9027ccb68c6880ae23bbbe01ed2c
ddubinin <dubinin.dmitry@mail.ru>
2022-10-31 12:23:31 +0300

    relocate app files

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // components
-import AppHeader from './components/app-header/app-header';
-import BurgerConstructor from './components/burger-constructor/burger-constructor';
-import BurgerIngredients from './components/burger-ingredients/burger-Ingredients';
+import AppHeader from '../app-header/app-header';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import BurgerIngredients from '../burger-ingredients/burger-Ingredients';
 // styles
 import styles from './app.module.css';
 // utils
-import ingredients from './utils/data';
+import ingredients from '../../utils/data';
 
 
 function App() {
