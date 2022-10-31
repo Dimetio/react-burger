6055c2828ef3c06e098a58c4fdf5921c01cbdee3
ddubinin <dubinin.dmitry@mail.ru>
2022-10-31 19:45:52 +0300

    delete unused component

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Logo, BurgerIcon, ListIcon, ProfileIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './app-header.module.css'
 
 export default function AppHeader() {
