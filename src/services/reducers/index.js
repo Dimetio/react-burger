339fd6cbf3b8f39c84b08f8e6f4cf221fca1a611
ddubinin <dubinin.dmitry@mail.ru>
2022-11-18 18:53:32 +0300

    feat: added new reducers

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -8,7 +8,16 @@ import {
   GET_INGREDIENTS_FAILED,
   GET_TARGET_INGREDIENT,
   DELETE_TARGET_INGREDIENT,
-} from '../actions'
+  ADD_INGREDIENT_CONSTRUCTOR,
+  DELETE_INGREDIENT_CONSTRUCTOR,
+  UPDATE_INGREDIENT_CONSTRUCTOR,
+  CLEAR_INGREDIENT_CONSTRUCTOR,
+  ADD_BUNS_CONSTRUCTOR,
+  GET_ORDER_REQUEST,
+  GET_ORDER_SUCCESS,
+  GET_ORDER_FAILED,
+  GET_TOTAL_PRICE,
+} from '../actions/index'
 
 // стейт всех ингредиентов
 const ingredientsInitialState = {
@@ -22,7 +31,20 @@ const targerIngredientInitialState = {
   ingredient: {}
 }
 
-const constructorInitialState = {}
+const constructorInitialState = {
+  bun: null,
+  ingredients: [],
+}
+
+const orderInitialState = {
+  order: {},
+  isLoading: false,
+  hasError: false,
+}
+
+const totalPriceInitialState = {
+  sum: 0
+}
 
 const ingredientsReducer = (state = ingredientsInitialState, action) => {
   switch (action.type) {
@@ -43,9 +65,8 @@ const ingredientsReducer = (state = ingredientsInitialState, action) => {
         isLoading: false,
           hasError: true,
       };
-    default: {
+    default:
       return state
-    }
   }
 }
 
@@ -61,18 +82,89 @@ const targetIngredientReducer = (state = targerIngredientInitialState, action) =
         ...state,
         ingredient: {},
       };
-    default: {
+    default:
       return state;
-    }
   }
 }
 
 const constructorReducer = (state = constructorInitialState, action) => {
+  switch (action.type) {
+    case ADD_INGREDIENT_CONSTRUCTOR:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, {
+          ...action.data,
+          _dndid: action.dndid
+        }],
+      };
+    case DELETE_INGREDIENT_CONSTRUCTOR:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((item) => item._dndid !== action.data._dndid),
+      };
+    case UPDATE_INGREDIENT_CONSTRUCTOR:
+      return {
+        ...state,
+        ingredients: [...action.data],
+      };
+    case CLEAR_INGREDIENT_CONSTRUCTOR:
+      return {
+        ...state,
+        bun: null,
+          ingredients: [],
+      };
+    case ADD_BUNS_CONSTRUCTOR:
+      return {
+        ...state,
+        bun: action.data,
+      };
+    default:
+      return state;
+  }
+}
 
+const orderReducer = (state = orderInitialState, action) => {
+  switch (action.type) {
+    case GET_ORDER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_ORDER_SUCCESS:
+      return {
+        ...state,
+        order: action.order,
+          osLoading: false,
+      };
+    case GET_ORDER_FAILED:
+      return {
+        ...state,
+        hasError: true,
+          osLoading: false,
+      };
+    default:
+      return state;
+  }
+}
+
+const totalPriceReducer = (state = totalPriceInitialState, action) => {
+  switch (action.type) {
+    case GET_TOTAL_PRICE:
+      const bunPrice = action.bun ? action.bun?.price * 2 : 0;
+      const total = action.ingredients.reduce((acc, item) => acc + item.price, 0)
+      return {
+        ...state,
+        sum: total + bunPrice,
+      };
+    default:
+      return state;
+  }
 }
 
 export const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
   targetIngredient: targetIngredientReducer,
   constructorIngredients: constructorReducer,
+  order: orderReducer,
+  totalPrice: totalPriceReducer,
 })
