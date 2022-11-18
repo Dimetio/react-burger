d957715c0bcdfc6dabc6264a5a03aaaa517225ec
ddubinin <dubinin.dmitry@mail.ru>
2022-11-18 18:53:41 +0300

    feat: added new actions

--- src/services/actions/index.js
+++ src/services/actions/index.js
@@ -1,3 +1,7 @@
+import {
+  v1 as random
+} from 'uuid';
+
 export const SET_TOTAL_PRICE = 'SET_TOTAL_PRICE';
 export const RESET_TOTAL_PRICE = 'RESET_TOTAL_PRICE';
 export const GET_TOTAL_PRICE = 'GET_TOTAL_PRICE';
@@ -12,6 +16,12 @@ export const GET_ORDER_FAILED = 'GET_ORDER_FAILED';
 
 export const GET_TARGET_INGREDIENT = 'GET_TARGET_INGREDIENT';
 export const DELETE_TARGET_INGREDIENT = 'DELETE_TARGET_INGREDIENT';
+
+export const ADD_INGREDIENT_CONSTRUCTOR = 'ADD_INGREDIENT_CONSTRUCTOR';
+export const DELETE_INGREDIENT_CONSTRUCTOR = 'DELETE_INGREDIENT_CONSTRUCTOR';
+export const UPDATE_INGREDIENT_CONSTRUCTOR = 'UPDATE_INGREDIENT_CONSTRUCTOR';
+export const CLEAR_INGREDIENT_CONSTRUCTOR = 'CLEAR_INGREDIENT_CONSTRUCTOR';
+export const ADD_BUNS_CONSTRUCTOR = 'ADD_BUNS_CONSTRUCTOR';
 
 function checkResponse(res) {
   if (res.ok) {
@@ -37,12 +47,98 @@ export const getIngredients = () => {
           type: GET_INGREDIENTS_SUCCESS,
           ingredients: data.data,
         })
-        .catch((err) => {
-          // console.log(err.message)
-          dispatch({
-            type: GET_INGREDIENTS_FAILED,
-          })
+      })
+      .catch((err) => {
+        console.log(err.message)
+        dispatch({
+          type: GET_INGREDIENTS_FAILED,
         })
       })
+  }
+}
+
+export const addIngredientConstructor = (data) => {
+  return {
+    type: ADD_INGREDIENT_CONSTRUCTOR,
+    data,
+    dndid: random(),
+  };
+};
+
+export const deleteIngredientConstructor = (data) => {
+  return {
+    type: DELETE_INGREDIENT_CONSTRUCTOR,
+    data,
+  };
+};
+
+export const updateIngredientConstructor = (data) => {
+  return {
+    type: UPDATE_INGREDIENT_CONSTRUCTOR,
+    data,
+  };
+};
+
+export const clearIngredientConstructor = () => {
+  return {
+    type: CLEAR_INGREDIENT_CONSTRUCTOR,
+  };
+};
+
+export const addBunsConstructor = (data) => {
+  return {
+    type: ADD_BUNS_CONSTRUCTOR,
+    data,
+  };
+};
+
+export const getOrder = (ingredientsId) => {
+  return function (dispatch) {
+    dispatch({
+      type: GET_ORDER_REQUEST
+    })
+    fetch('https://norma.nomoreparties.space/api/orders', {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json",
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          ingredients: ingredientsId
+        })
+      })
+      .then(checkResponse)
+      .then((data) => {
+        dispatch({
+            type: GET_ORDER_SUCCESS,
+            order: data.order,
+          })
+          .catch((err) => {
+            dispatch({
+              type: GET_ORDER_FAILED,
+            })
+          })
+      })
+  }
+}
+
+export const getTotalPrice = (ingredients, bun) => {
+  return {
+    type: GET_TOTAL_PRICE,
+    bun,
+    ingredients,
+  }
+}
+
+export const getTargetIngredient = (ingredient) => {
+  return {
+    type: GET_TARGET_INGREDIENT,
+    ingredient
+  }
+}
+
+export const deleteTargetIngredient = () => {
+  return {
+    type: DELETE_TARGET_INGREDIENT
   }
 }
