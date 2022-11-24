a57ee49def8b23ef3e7abae6b56ea925e6a7dec4
ddubinin <dubinin.dmitry@mail.ru>
2022-11-24 23:24:37 +0300

    feat: create login page

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { useState } from 'react'
+import Form from '../form/form'
+import InputComponent from '../input/input'
+import { Link } from 'react-router-dom'
+import styles from './page.module.css'
 
 export default function Login() {
+  const [showPassord, setShowPassword] = useState(false)
+
+  function onIconClick() {
+    setShowPassword(!showPassord)
+  }
+
   return (
-    <div>Login</div>
+    <section className={styles.section}>
+      <Form
+        title={'Вход'}
+        buttonText={'Войти'}
+      >
+        <InputComponent
+          name='email'
+          isIcon={false}
+          placeholder='E-mail'
+          type='email'
+        />
+
+        <InputComponent
+          name='password'
+          placeholder='password'
+          icon={showPassord ? 'HideIcon' : 'ShowIcon'}
+          onIconClick={onIconClick}
+          type={showPassord ? 'text' : 'password'}
+        />
+      </Form>
+
+      <p className="text text_type_main-default mb-4" style={{ textAlign: 'center' }}>
+        Вы — новый пользователь?
+        <Link
+          to='/register'
+          className='ml-2 text text_type_main-default'
+        >Зарегистрироваться</Link>
+      </p>
+
+      <p className="text text_type_main-default mb-4" style={{ textAlign: 'center' }}>
+        Забыли пароль?
+        <Link
+          to='/forgot-password'
+          className='ml-2 text text_type_main-default'
+        >Восстановить пароль</Link>
+      </p>
+    </section>
   )
 }
