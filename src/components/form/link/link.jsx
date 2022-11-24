3c8234539bcdf3f739e1b987653e57b9b7587b66
ddubinin <dubinin.dmitry@mail.ru>
2022-11-25 01:08:38 +0300

    update component link

--- src/components/form/link/link.jsx
+++ src/components/form/link/link.jsx
@@ -7,11 +7,13 @@ export default function CustomLink({ text, url, linkText }) {
       <p className='text text_type_main-default'>
         {text}
       </p>
+
       <Link
         to={url}
-        className={`${styles.link} text text_type_main-default ml-4`}
-      >{linkText}</Link>
-
+        className={`${styles.link} text text_type_main-default ml-2`}
+      >
+        {linkText}
+      </Link>
     </div>
   )
 }
