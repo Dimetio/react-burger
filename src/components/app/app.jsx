e187257f27f4652b43ff764a3d695e86e9593c6c
ddubinin <dubinin.dmitry@mail.ru>
2022-11-24 16:25:45 +0300

    feat: create routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,10 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // components
 import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-Ingredients';
+// pages
+import { ForgotPassword, Login, NotFound404, Profile, Register, ResetPassword, TargetIngredient } from '../pages/index'
 // styles
 import styles from './app.module.css';
 
@@ -9,17 +12,43 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
-
   return (
-    <div className="App">
-      <AppHeader />
-      <main className={styles.main}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </DndProvider>
-      </main>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <div className="App">
+            <AppHeader />
+            <main className={styles.main}>
+              <DndProvider backend={HTML5Backend}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+              </DndProvider>
+            </main>
+          </div>
+        </Route>
+        <Route path="/login" exact={true}>
+          <Login />
+        </Route>
+        <Route path="/register" exact={true}>
+          <Register />
+        </Route>
+        <Route path="/forgot-password" exact={true}>
+          <ForgotPassword />
+        </Route>
+        <Route path="/reset-password" exact={true}>
+          <ResetPassword />
+        </Route>
+        <Route path="/profile" exact={true}>
+          <Profile />
+        </Route>
+        <Route path={`/ingredients/:id`} exact={true}>
+          <TargetIngredient />
+        </Route>
+        <Route>
+          <NotFound404 />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
