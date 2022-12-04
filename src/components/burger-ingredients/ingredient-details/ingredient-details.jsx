aaf86a7b6e89bf7606c0b2d6487bec4480e93250
ddubinin <dubinin.dmitry@mail.ru>
2022-12-04 16:36:48 +0300

    fix: delete Link in modal

--- src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
+++ src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
@@ -14,31 +14,29 @@ export default function IngredientDetails() {
   const ingredient = ingredients.find(i => i._id === id)
 
   return (
-    <Link className={styles.link}>
-      <article className={`${styles.wrap} ml-10 mr-10 mb-15`}>
-        <div className={`${styles.image} mb-4`}>
-          <img src={ingredient?.image_large} alt={ingredient?.name} />
-        </div>
-        <p className="text text_type_main-medium mb-8">{ingredient?.name}</p>
-        <ul className={`${styles.list} text text_color_inactive`}>
-          <li className={styles.list_item}>
-            <span className="text_type_main-default">Калории,ккал</span>
-            <span className="text_type_digits-default">{ingredient?.calories}</span>
-          </li>
-          <li className={styles.list_item}>
-            <span className="text_type_main-default">Белки, г</span>
-            <span className="text_type_digits-default">{ingredient?.proteins}</span>
-          </li>
-          <li className={styles.list_item}>
-            <span className="text_type_main-default">Жиры, г</span>
-            <span className="text_type_digits-default">{ingredient?.fat}</span>
-          </li>
-          <li className={styles.list_item}>
-            <span className="text_type_main-default">Углеводы, г</span>
-            <span className="text_type_digits-default">{ingredient?.carbohydrates}</span>
-          </li>
-        </ul>
-      </article>
-    </Link>
+    <article className={`${styles.wrap} ml-10 mr-10 mb-15`}>
+      <div className={`${styles.image} mb-4`}>
+        <img src={ingredient?.image_large} alt={ingredient?.name} />
+      </div>
+      <p className="text text_type_main-medium mb-8">{ingredient?.name}</p>
+      <ul className={`${styles.list} text text_color_inactive`}>
+        <li className={styles.list_item}>
+          <span className="text_type_main-default">Калории,ккал</span>
+          <span className="text_type_digits-default">{ingredient?.calories}</span>
+        </li>
+        <li className={styles.list_item}>
+          <span className="text_type_main-default">Белки, г</span>
+          <span className="text_type_digits-default">{ingredient?.proteins}</span>
+        </li>
+        <li className={styles.list_item}>
+          <span className="text_type_main-default">Жиры, г</span>
+          <span className="text_type_digits-default">{ingredient?.fat}</span>
+        </li>
+        <li className={styles.list_item}>
+          <span className="text_type_main-default">Углеводы, г</span>
+          <span className="text_type_digits-default">{ingredient?.carbohydrates}</span>
+        </li>
+      </ul>
+    </article>
   )
 }
