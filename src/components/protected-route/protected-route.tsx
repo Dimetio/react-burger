474f48966bb6de03d45211ffe80d1f9974a0d724
ddubinin <dubinin.dmitry@mail.ru>
2022-12-16 17:45:50 +0300

    feat: typing ProtectedRoute

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,38 @@
+import { useEffect, FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import { getUserAction } from "../../services/actions/auth";
+import { getCookie } from "../../utils/cookie";
+
+type TProtected = {
+  children: JSX.Element;
+  anonymous?: boolean;
+};
+
+const ProtectedRoute: FC<TProtected> = ({ children, anonymous = false }) => {
+  const user = useSelector((store: any) => store.auth.user);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (getCookie("accessToken")) {
+      dispatch<any>(getUserAction());
+    }
+  }, [dispatch]);
+
+  const from = location.state?.from || "/";
+
+  // не пускает на страницу Login, когда авторизован
+  if (user && anonymous) {
+    return <Navigate to={from} />;
+  }
+
+  // редиректит на Login, когда не авторизован
+  if (!user && !anonymous) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
