7a1491ceb63e405c9e618dc71f36b6492f2f016c
ddubinin <dubinin.dmitry@mail.ru>
2022-12-16 17:46:02 +0300

    feat: update App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,42 +1,49 @@
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 // components
-import AppHeader from '../app-header/app-header';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import BurgerIngredients from '../burger-ingredients/burger-Ingredients';
-import Orders from '../orders/orders';
-import Profile from '../profile/profile';
+import AppHeader from "../app-header/app-header";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
+import BurgerIngredients from "../burger-ingredients/burger-Ingredients";
+import Orders from "../orders/orders";
+import Profile from "../profile/profile";
 // pages
-import { ForgotPassword, Login, NotFound404, ProfilePage, Register, ResetPassword, TargetIngredient } from '../../pages/index'
+import {
+  ForgotPassword,
+  Login,
+  NotFound404,
+  ProfilePage,
+  Register,
+  ResetPassword,
+  TargetIngredient,
+} from "../../pages/index";
 // styles
-import styles from './app.module.css';
+import styles from "./app.module.css";
 
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { ProtectedRoute } from '../protected-route/protected-route';
-import Modal from '../modal/modal';
-import IngredientDetails from '../burger-ingredients/ingredient-details/ingredient-details';
-import { useEffect } from 'react';
-import { getIngredients, getUserAction } from '../../services/actions';
-import { useDispatch, useSelector } from 'react-redux';
-
+import ProtectedRoute from "../protected-route/protected-route";
+import Modal from "../modal/modal";
+import IngredientDetails from "../burger-ingredients/ingredient-details/ingredient-details";
+import { useEffect } from "react";
+import { getIngredients, getUserAction } from "../../services/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const location = useLocation();
   const background = location.state && location.state.background;
-  const user = useSelector((store: any) => store.auth.user)
+  const user = useSelector((store: any) => store.auth.user);
 
   function onDismiss() {
-    navigate(-1)
+    navigate(-1);
   }
 
   useEffect(() => {
-    dispatch<any>(getIngredients())
+    dispatch<any>(getIngredients());
     if (user) {
-      dispatch<any>(getUserAction())
+      dispatch<any>(getUserAction());
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -89,31 +96,37 @@ function App() {
           </Route>
 
           <Route path={`/ingredients/:id`} element={<TargetIngredient />} />
-          <Route path='*' element={<NotFound404 />} />
-          <Route path="/" element={
-            <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </DndProvider>
-          } />
+          <Route path="*" element={<NotFound404 />} />
+          <Route
+            path="/"
+            element={
+              <DndProvider backend={HTML5Backend}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+              </DndProvider>
+            }
+          />
         </Routes>
 
         {background && (
           <Routes>
-            <Route path="ingredients/:id" element={
-              <Modal
-                title={'Детали ингредиента'}
-                closeModal={onDismiss}
-                isOpened={true}
-              >
-                <IngredientDetails />
-              </Modal>
-            } />
+            <Route
+              path="ingredients/:id"
+              element={
+                <Modal
+                  title={"Детали ингредиента"}
+                  closeModal={onDismiss}
+                  isOpened={true}
+                >
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
           </Routes>
         )}
       </main>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default App;
