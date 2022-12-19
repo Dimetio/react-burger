b6e063d94b86b510c238612b23a790874cc46dad
ddubinin <dubinin.dmitry@mail.ru>
2022-12-19 18:10:43 +0300

    feat: typing cookies

--- src/services/actions/auth.js
+++ src/services/actions/auth.js
@@ -3,7 +3,7 @@ import * as api from '../../utils/api';
 import {
   deleteCookie,
   setCookie
-} from '../../utils/cookie.js';
+} from '../../utils/cookie.ts';
 
 export const REGISTER_REQUEST = 'REGISTER_REQUEST';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
