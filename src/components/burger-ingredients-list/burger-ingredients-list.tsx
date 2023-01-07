73e535ea4c0fe63ab4edef5af44312f6857cac6e
ddubinin <dubinin.dmitry@mail.ru>
2023-01-07 19:10:42 +0300

    feat: typing burger-ingredient-list

--- src/components/burger-ingredients-list/burger-ingredients-list.tsx
+++ src/components/burger-ingredients-list/burger-ingredients-list.tsx
@@ -3,27 +3,28 @@ import { useDispatch, useSelector } from "../../services/hooks";
 import BurgerIngredientsItem from "./burger-ingredients-item/burger-ingredients-item";
 import { updateIngredientConstructor } from "../../services/actions";
 
-import { TConstructorIngredient } from "../../utils/types";
-
 export default function BurgerIngredientsList(): JSX.Element {
   const { ingredients } = useSelector((store) => store.constructorIngredients);
   const dispatch = useDispatch();
 
   const moveCard = useCallback(
     (dragIndex: number, hoverIndex: number) => {
-      const dragCard = ingredients[dragIndex];
-      const newCards = [...ingredients];
-      newCards.splice(dragIndex, 1);
-      newCards.splice(hoverIndex, 0, dragCard);
-
-      dispatch(updateIngredientConstructor(newCards));
+      dispatch(updateIngredientConstructor(dragIndex, hoverIndex));
     },
     [ingredients, dispatch]
   );
 
-  return ingredients.map((item: TConstructorIngredient, index: number) => (
-    <li key={item._dndid}>
-      <BurgerIngredientsItem item={item} index={index} moveCard={moveCard} />
-    </li>
-  ));
+  return (
+    <>
+      {ingredients.map((item, index) => (
+        <li key={item._dndid}>
+          <BurgerIngredientsItem
+            item={item}
+            index={index}
+            moveCard={moveCard}
+          />
+        </li>
+      ))}
+    </>
+  );
 }
