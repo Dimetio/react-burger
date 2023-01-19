01b74a0886c40a57ada994c9406900b128dbe123
ddubinin <dubinin.dmitry@mail.ru>
2023-01-19 16:14:50 +0300

    feat: ready to deploy gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./services/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
